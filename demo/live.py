8bea4b0cf55c0434e600a51086d4aee4fa506e75
Xinliang Zhang <zhangxinliang_mit@163.com>
2019-03-21 20:22:47 +0800

    update

--- demo/live.py
+++ demo/live.py
@@ -7,12 +7,12 @@ from imutils.video import FPS, WebcamVideoStream
 import argparse
 
 parser = argparse.ArgumentParser(description='Single Shot MultiBox Detection')
-parser.add_argument('--weights', default='weights/ssd300_COCO_235000.pth',
+parser.add_argument('--weights', default='weights/ssd300_COCO_255000.pth',
                     type=str, help='Trained state_dict file path')
 parser.add_argument('--cuda', default=True, type=bool,
                     help='Use cuda in live demo')
 parser.add_argument('--num_class',default=8,type=int)
-parser.add_argument('--threhold',default=0.6,type=float)
+parser.add_argument('--threhold',default=0.4,type=float)
 args = parser.parse_args()
 
 COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
@@ -39,8 +39,11 @@ def cv2_demo(net, transform):
                               (int(pt[2]), int(pt[3])),
                               COLORS[i % 3], 2)
                 trust_level = str(detections[0, i, j, 0]).split('tensor')[1]
-                cv2.putText(frame, labelmap[i - 1]+trust_level, (int(pt[0]), int(pt[1])),
-                            FONT, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                class_name = labelmap[i-1]
+                # if class_name == 'holothurian':
+                #     class_name ="海参"
+                cv2.putText(frame, class_name+trust_level, (int(pt[0]), int(pt[1])),
+                            FONT, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
                 j += 1
         return frame
 
